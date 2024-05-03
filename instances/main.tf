9c051bafb643ca882eb77f877eaa07e47ea79b4b
Robin Norwood <rnorwood@hashicorp.com>
2024-05-03 15:00:29 -0500

    Updates from feedback

--- instances/main.tf
+++ instances/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
 }
@@ -17,7 +7,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*20*-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
@@ -98,7 +88,7 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.subnet_public.id
   vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
   associate_public_ip_address = true
-  user_data                   = file("scripts/add-ssh-web-app.yaml")
+  user_data                   = file("../scripts/add-ssh-web-app.yaml")
 
   tags = {
     Name = "Learn-CloudInit"
